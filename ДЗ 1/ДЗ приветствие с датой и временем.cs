2627d0016543dcb5985a045a376e41ebf5ec40fb
Elizar706 <v.a.elizar@mail.ru>
2021-04-26 23:17:34 +0300

    Первая домашняя работа

---
+++ ДЗ 1/ДЗ приветствие с датой и временем.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date1 = new DateTime();
+            Console.Write("Как тебя зовут? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Привет, " + name + "! " + "Сегодня" + " {" + DateTime.Now + "}");
+        }
+    }
+}
